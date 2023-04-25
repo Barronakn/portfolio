4d5a6c385ebaeb0686bfdb8effab1353433658d6
Barron <barronakn@gmail.com>
2023-04-25 18:33:59 +0100

    Mise en ligne

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/portfolio" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </div>
