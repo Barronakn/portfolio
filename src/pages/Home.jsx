d06c657c6b4217e32db398e56f0a14e791310e83
Barron <barronakn@gmail.com>
2023-04-25 18:26:43 +0100

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,68 @@
+import { NavLink } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h1 className="name">Jean-Barron ALOKPON.</h1>
+      <p className="desc">Voici quelques uns de mes réalisations.</p>
+      <div className="projet">
+        <div className="p">
+          <div>
+            <img
+              src="/src/assets/Capture d’écran du 2023-04-07 08-34-47.png"
+              alt="adept"
+            />
+            <NavLink to="https://barronakn.github.io/Adept/">Adept</NavLink>
+          </div>
+          <div>
+            <img
+              src="/src/assets/Capture d’écran du 2023-04-07 10-00-39.png"
+              alt="aknmode"
+            />
+            <NavLink to="https://barronakn.github.io/aknmode/">
+              Akn mode
+            </NavLink>
+          </div>
+        </div>
+        <div className="p">
+          <div>
+            <img
+              src="/src/assets/Capture d’écran du 2023-04-07 10-11-54.png"
+              alt="boutique"
+            />
+            <NavLink to="https://barronakn.github.io/boutiquemode/">
+              Boutique
+            </NavLink>
+          </div>
+          <div>
+            <img
+              src="/src/assets/Capture d’écran du 2023-04-07 10-14-15.png"
+              alt="skillhub"
+            />
+            <NavLink to="https://barronakn.github.io/Competence-Center/">
+              Skillhub
+            </NavLink>
+          </div>
+        </div>
+        <div className="p">
+          <div>
+            <img
+              src="/src/assets/Capture d’écran du 2023-04-07 10-37-46.png"
+              alt="sigma"
+            />
+            <NavLink to="https://barronakn.github.io/Sigma/">Sigma</NavLink>
+          </div>
+          <div>
+            <img
+              src="/src/assets/Capture d’écran du 2023-04-07 11-29-42.png"
+              alt="fylla"
+            />
+            <NavLink to="https://barronakn.github.io/Fylla/">Fylla</NavLink>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
